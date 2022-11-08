265c4afbbe5b89e6cf1a16e340595baf908d1265
Ousman-Busari <usman.traid@gmail.com>
2022-11-08 11:23:40 -0500

    quick sort for array of integers

---
+++ 3-quick_sort.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+
+int lomuto_partition(int *array, size_t size, int start, int end);
+void lomuto_sort(int *array, size_t size, int start, int end);
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ *              using the quick sort algorithm
+ * @array: the array of integers to be sorted
+ * @size: size of the array
+ *
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+
+	if (!array || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
+
+/**
+ * lomuto_sort - sorts an array of integers within a
+ *               certain range of index
+ * @array: the array to be sorted
+ * @size: size of the array
+ * @start: index of where to start the sorting
+ * @end: index of where to end the sorting
+ *
+ * Return: Nothing
+ */
+void lomuto_sort(int *array, size_t size, int start, int end)
+{
+	int p_index;
+
+	if (start < end)
+	{
+		p_index = lomuto_partition(array, size, start, end);
+		lomuto_sort(array, size, start, p_index - 1);
+		lomuto_sort(array, size, p_index + 1, end);
+	}
+}
+
+/**
+ * partition - uses the last element of an array as the pivot for
+ *             partitioning an array withi a range of index
+ * @array: array of integers to be partitioned
+ * @size: size of the array
+ * @start: index where to start the partitioning
+ * @end: where to end the partioning
+ *
+ * Return: right index partiton element use as pivot
+ */
+int lomuto_partition(int *array, size_t size, int start, int end)
+{
+	int i, holder, p_index = start;
+	int pivot;
+
+	pivot = array[end];
+	for (i = start; i < end; i++)
+	{
+		if (array[i] < pivot)
+		{
+			if (array[i] < array[p_index])
+			{
+				holder = array[i];
+				array[i] = array[p_index];
+				array[p_index] = holder;
+       				print_array(array, size);
+			}
+			p_index++;
+		}
+	}
+
+	if (array[p_index] > pivot)
+	{
+		holder = array[p_index];
+		array[p_index] = pivot;
+		array[end] = holder;
+		print_array(array, size);
+	}
+	return (p_index);
+}
